9878133a460c2cd5e21f3eb295f1d9aa2edf8045
philip.champkin <philip.champkin@uk.bnpparibas.com>
2020-08-19 22:58:48 +0100

    add routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,18 +9,9 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  constructor(
-    private heroService: HeroService,
-    private messageService: MessageService
-    ) {}
-
   heroes: Hero[];
-  selectedHero : Hero;
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add('HeroesComponent', `Selected hero id=${hero.id}`);
-  }
+  constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
     this.getHeroes();
